cf0c66636a33eef6bf0b00eec7c6edeb80914a7a
Andrea Mantovani <andreamnt94@gmail.com>
2015-12-17 00:58:40 +0100

    Define interface MailBox

--- src/main/java/it/unipd/math/pcd/actors/MailBox.java
+++ src/main/java/it/unipd/math/pcd/actors/MailBox.java
@@ -1,7 +1,13 @@
 package it.unipd.math.pcd.actors;
 
+
 /**
  * Created by amantova on 16/12/15.
  */
-public interface MailBox {
+public interface MailBox<T extends Message,U extends ActorRef<T>>
+{
+    boolean isEmpty();
+
+    HeadMail<T,U> pop();
+    void append( T message, U send );
 }
