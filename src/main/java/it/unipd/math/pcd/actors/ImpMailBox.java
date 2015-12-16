b77362edca89e7f3d3469c1a1e41ea403d3359da
Andrea Mantovani <andreamnt94@gmail.com>
2015-12-17 00:57:15 +0100

    Implement MailBox no thread-safe

--- src/main/java/it/unipd/math/pcd/actors/ImpMailBox.java
+++ src/main/java/it/unipd/math/pcd/actors/ImpMailBox.java
@@ -1,7 +1,40 @@
 package it.unipd.math.pcd.actors;
 
+import java.util.LinkedList;
+
 /**
  * Created by amantova on 16/12/15.
  */
-public class ImpMailBox {
+public final class ImpMailBox<T extends Message,U extends ActorRef<T>> implements MailBox<T,U>
+{
+    private LinkedList<HeadMail<T,U>> queue_ = new LinkedList<>();
+    /**
+     * Check if queue_ is empty
+     * @return True if size of queue_ is 0, other False
+     */
+    @Override
+    public boolean isEmpty()
+    {
+        return ( queue_.size() == 0 );
+    }
+
+    /**
+     * Store message in the tail of queue
+     * @param message The message of storage in the queue
+     */
+    @Override
+    public void append( T message, U send )
+    {
+        queue_.addLast( new HeadMail<>( message, send ) );
+    }
+
+    /**
+     * Get the head message in the queue
+     * @return message extract
+     */
+    @Override
+    public HeadMail<T,U> pop()
+    {
+        return queue_.pollFirst();
+    }
 }
