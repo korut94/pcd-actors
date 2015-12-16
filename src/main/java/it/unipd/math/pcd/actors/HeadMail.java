723c83041374808fe07561316f4ff9886f44b2d1
Andrea Mantovani <andreamnt94@gmail.com>
2015-12-17 00:04:14 +0100

    Add TODO Any requirements will write here.

---
+++ src/main/java/it/unipd/math/pcd/actors/HeadMail.java
@@ -0,0 +1,12 @@
+package it.unipd.math.pcd.actors;
+
+/**
+ * Created by amantova on 16/12/15.
+ */
+public class HeadMail<T extends Message, U extends ActorRef>
+{
+    T message_;
+    U sender_;
+
+    HeadMail(  )
+}
---
+++ src/main/java/it/unipd/math/pcd/actors/ImpMailBox.java
@@ -0,0 +1,7 @@
+package it.unipd.math.pcd.actors;
+
+/**
+ * Created by amantova on 16/12/15.
+ */
+public class ImpMailBox {
+}
