97ec3b5c8b7189fb7ea2a53bc107c634ccd7307a
Andrea Mantovani <andreamnt94@gmail.com>
2015-12-18 00:55:35 +0100

    Declare param type T in bound ActorRef of U

--- src/main/java/it/unipd/math/pcd/actors/HeadMail.java
+++ src/main/java/it/unipd/math/pcd/actors/HeadMail.java
@@ -3,7 +3,7 @@ package it.unipd.math.pcd.actors;
 /**
  * Created by amantova on 16/12/15.
  */
-public class HeadMail<T extends Message, U extends ActorRef>
+public class HeadMail<T extends Message, U extends ActorRef<T>>
 {
     T message_;
     U sender_;
