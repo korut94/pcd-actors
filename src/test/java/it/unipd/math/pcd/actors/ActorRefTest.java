2e0804d81b71a29fa0216688d5266800426b9b2a
Andrea Mantovani <andreamnt94@gmail.com>
2015-12-22 12:32:50 +0100

    Sleep follow stop

--- src/test/java/it/unipd/math/pcd/actors/ActorRefTest.java
+++ src/test/java/it/unipd/math/pcd/actors/ActorRefTest.java
@@ -111,6 +111,15 @@ public class ActorRefTest {
         }
 
         system.stop();
+
+        try
+        {
+            Thread.sleep( 2000 );
+        }
+        catch( InterruptedException e )
+        {
+
+        }
     }
 }
 
