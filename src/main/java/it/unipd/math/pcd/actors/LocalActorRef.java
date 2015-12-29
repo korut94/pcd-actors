d3ccce75a937030271d569c16c1b2a77997f7a0e
Andrea Mantovani <andreamnt94@gmail.com>
2015-12-29 22:44:15 +0100

    Remove old import

--- src/main/java/it/unipd/math/pcd/actors/LocalActorRef.java
+++ src/main/java/it/unipd/math/pcd/actors/LocalActorRef.java
@@ -1,32 +1,13 @@
 package it.unipd.math.pcd.actors;
 
-import com.sun.istack.internal.NotNull;
-
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 /**
  * Created by amantova on 16/12/15.
  */
-public final class LocalActorRef<T extends Message> implements ActorRef<T>
+public final class LocalActorRef<T extends Message> extends ImpActorRef<T>
 {
-    private AbsActorSystem system_;
-
     public LocalActorRef( AbsActorSystem system )
     {
-        system_ = system;
-    }
-
-    /**
-     * Check if actorRef references is same
-     * @param other ActorRef
-     * @return return 0 if same else -1
-     */
-    @Override
-    public int compareTo( ActorRef other )
-    {
-        return ( this == other ) ? 0 : -1;
+        super( system );
     }
 
     /**
